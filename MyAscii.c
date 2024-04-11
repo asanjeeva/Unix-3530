24d89ff79b68d549767ebfc49e2f1dfec79282e4
Archie Sanjeeva <archit.nsanjeeva@gmail.com>
2024-04-10 19:32:35 -0500

    modified file

--- MyAscii.c
+++ MyAscii.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {  
 	    char c;
-	        printf("Enter a character: ");
+	        printf("Enter a character to display Ascii code: ");
 		    scanf("%c", &c);  
 		        
 		        printf("ASCII value of %c = %d", c, c);
